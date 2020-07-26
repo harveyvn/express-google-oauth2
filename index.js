6acb332af2bb9e1642b0a44e369ec8f75c086d44
Harvey <ngvuong.me@outlook.com>
2020-07-26 23:37:30 +0200

    Enable Google OAuth API and ask what access we want to have inside scope parameter

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oath20').Strategy;
+const keys = require('./config/key');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+  }),
+  (accessToken) => {
+    console.log(accessToken);
+  },
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
 
 app.get('/', (req, res) => {
   res.send({ hello: 'world' });
